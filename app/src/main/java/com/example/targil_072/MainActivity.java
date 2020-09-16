f98bac3025211f5fa39b25e7f5923c4ff6ccbcdd
yp6497 <59597619+yp6497@users.noreply.github.com>
2020-09-16 23:58:19 +0300

    design

--- app/src/main/java/com/example/targil_072/MainActivity.java
+++ app/src/main/java/com/example/targil_072/MainActivity.java
@@ -1,22 +1,84 @@
 package com.example.targil_072;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    String st1,st2,st3,x1,x2;
+    TextView t1,t2;
+    EditText ed1,ed2,ed3;
+    float a,b,c;
+    double ans1,ans2;
+    //boolean isR=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1=findViewById(R.id.ed1);
+        ed2=findViewById(R.id.ed2);
+        ed3=findViewById(R.id.ed3);
+        t1=findViewById(R.id.t1);
+        t2=findViewById(R.id.t2);
+    }
+
+    protected void OnActivityResult(int source, int good, @Nullable Intent data_back){
+        if(data_back!=null){
+            ans1=data_back.getDoubleExtra("a1",0);
+            ans2=data_back.getDoubleExtra("a2",0);
+            t1.setText("x1:"+ans1);
+            t2.setText("x1:"+ans2);
+        }
+
     }
 
     public void pitaron(View view) {
-        Intent si=new Intent(this,pitaron.class);
-        si.putExtra("a",12);
-        startActivity(si);
+
+        st1 = ed1.getText().toString();
+        st2 = ed2.getText().toString();
+        st3 = ed3.getText().toString();
+        if (st1.isEmpty() || st2.isEmpty() || st3.isEmpty()) {
+            Toast.makeText(this, "please enter a number", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            a = Float.parseFloat(st1);
+            b = Float.parseFloat(st2);
+            c = Float.parseFloat(st3);
+            Intent si = new Intent(this, pitaron.class);
+            si.putExtra("a", a);
+            si.putExtra("b", b);
+            si.putExtra("c", c);
+            startActivity(si);
+        }
+    }
+
+    public void RandomN(View view) {
+        Random rnd=new Random();
+        a=(-100)+rnd.nextFloat()*(100-(-100));
+        if(rnd.nextBoolean()) {
+            a = a *-1;
+        }
+        b=(-100)+rnd.nextFloat()*(100-(-100));
+        if(rnd.nextBoolean()) {
+            b = b *-1;
+        }
+        c=(-100)+rnd.nextFloat()*(100-(-100));
+        if(rnd.nextBoolean()) {
+            c = c*-1;
+        }
+
+        ed1.setText(""+a);
+        ed2.setText(""+b);
+        ed3.setText(""+c);
     }
 }
--- app/src/main/java/com/example/targil_072/pitaron.java
+++ app/src/main/java/com/example/targil_072/pitaron.java
@@ -2,13 +2,83 @@ package com.example.targil_072;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class pitaron extends AppCompatActivity {
 
+    float a,b,c,nos;
+    double ans1,ans2;
+    WebView wV;
+    TextView t1,t2;
+    String aS, bS, cS, url;
+    //Intent mi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pitaron);
+
+        t1=findViewById(R.id.t1);
+        t2=findViewById(R.id.t2);
+        wV=findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+
+        pitaronn();
+        hisov();
+
+    }
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView ww, String url) {
+            ww.loadUrl(url);
+            return true;
+        }
+    }
+
+    private void hisov() {
+        nos = (b * b) - 4 * a * c;
+        if ((nos) < 0) {
+            t1.setText("no answer");
+            t2.setText("no answer");
+        }
+        else{
+        ans1 = (-b - (Math.sqrt(nos))) / (2 * a);
+        ans2 = (-b + (Math.sqrt(nos))) / (2 * a);
+        t1.setText("x1=" + ans1);
+        t2.setText("x2=" + ans2);
+        }
+    }
+
+    public void pitaronn() {
+        Intent gi=getIntent();
+        a = gi.getFloatExtra("a", 1);
+        b = gi.getFloatExtra("b", 1);
+        c = gi.getFloatExtra("c", 1);
+        aS = Float.toString(a);
+        bS = Float.toString(b);
+        cS = Float.toString(c);
+        if (bS.isEmpty()) {
+            url = "https://www.google.com/search?q=" + aS + "x%5E2%2B" + cS;
+        } else if (cS.isEmpty()) {
+            url = "https://www.google.com/search?q=" + aS + "x%5E2%2B" + bS + "x";
+        }else if (bS.isEmpty() && cS.isEmpty()) {
+            url = "https://www.google.com/search?q=" + aS + "x%5E2";
+        }
+        else {
+            url = "https://www.google.com/search?q=" + aS + "x%5E2%2B" + bS + "x%2B"+cS;
+        }
+        wV.loadUrl(url);
+    }
+
+    public void returnn(View view) {
+        Intent gi=getIntent();
+        gi.putExtra("a1",ans1);
+        gi.putExtra("a2",ans2);
+        setResult(RESULT_OK,gi);
+        finish();
     }
 }
